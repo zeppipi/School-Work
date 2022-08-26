583b6e760fea5034087f6fce88a8a9cb19d17bc9
zeppipi <107682921+zeppipi@users.noreply.github.com>
2022-08-26 17:37:05 +0800

    Game engine has been made
    The Asteroid FRP has been reverse engineered enough to give the player the ability to move!

--- FIT 2102/assignment1/src/main.ts
+++ FIT 2102/assignment1/src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 import { interval, fromEvent, Observable, merge } from 'rxjs'
-import { map, filter, subscribeOn, scan } from "rxjs/operators";
+import { map, filter, subscribeOn, scan, elementAt } from "rxjs/operators";
 
 function main() {
   /**
@@ -24,14 +24,15 @@ function main() {
 
   // Store the possible game actions
   class Move {constructor(public readonly x_move: number, readonly y_move: number) {} }  // A move state
-  class Tick {constructor(public readonly time: number) {} }                    // A clock
+  class Tick {constructor(public readonly elapsed: number) {} }                             // A clock
   
   // Store constants
   const Constants = 
   {
     CanvasSize: 600,                // The canvas you are working with is 600x600
-    playerSpawnPoint: [300, 550],   // Where the player will spawn
-    playerMoveDis: 20,              // Store the distance the player should move
+    playerSpawnPoint: [300, 500],   // Where the player will spawn
+    playerMoveDis: 40,              // Store the distance the player should move
+    defaultEnemyMoveDis: 1          // Store the distance a normal enemy should move
   } as const;
   
   /**
@@ -41,11 +42,18 @@ function main() {
   type Circle = Readonly<{pos_x:number, pos_y:number}>
 
   /**
+   * All possible info of any square objects
+   */
+  type Rectangle = Readonly<{pos_x: number, pos_y: number}>
+
+  /**
    * All possible info of any objects
    * Code derived from the Astreoid FRP
    */
-  interface IBody extends Circle{
-    ID: string;
+  interface IBody extends Circle, Rectangle{
+    ID: string,
+    shapeID: string,
+    speed: number;
   }
 
   // Body object, made from its interface
@@ -58,8 +66,24 @@ function main() {
   {
     return{
       ID: "frog",
+      shapeID: "ellipse",
       pos_x: Constants.playerSpawnPoint[0],
       pos_y: Constants.playerSpawnPoint[1],
+      speed: 0
+    };
+  }
+
+  /**
+   * Function that creates the enemies!
+   */
+  function createEnemy(xPosition: number, yPosition: number, movementSpeed: number): Body
+  {
+    return{
+      ID: "car",
+      shapeID: "rect",
+      pos_x: xPosition,
+      pos_y: yPosition,
+      speed: movementSpeed
     };
   }
   
@@ -69,6 +93,7 @@ function main() {
   type State = Readonly<{
     time:number,
     frog:Body,
+    cars:ReadonlyArray<Body>,
     gameOver:boolean
   }>
 
@@ -79,11 +104,12 @@ function main() {
   {
     time: 0,
     frog: createPlayer(),
+    cars: [createEnemy(0, 350, 1)],
     gameOver: false
   }
 
   /**
-   * What to do on each state
+   * What happens on each state in relation to the events
    */
   const currentState = (curState: State, curEvent: Move | Tick) =>
     curEvent instanceof Move ?
@@ -94,26 +120,29 @@ function main() {
           pos_y: curState.frog.pos_y + curEvent.y_move
         }
       }
-      : tick(curState)//Make this empty for now
-
+      : tick(curState, curEvent.elapsed)//Make this empty for now
+  
   /**
-   * Make this empty for now
+   * What happens on each state in relation to the whole game
    */
-  const tick = (curState: State) =>
+  const tick = (curState: State , elapsed: number) =>
   {
-    return {...curState};
+    return {...curState,
+      cars: curState.cars.map(moveBody),
+      time: elapsed
+    };
   }
 
   //Player Move logic starts here
   /**
    * All possible keys
    */
-  type Key = 'ArrowLeft' | 'ArrowRight' | 'ArrowUp' | 'ArrowDown' | 'a' | 'd' | 'w' | 's'
+  type Key = 'KeyA' | 'KeyD' | 'KeyW' | 'KeyS' 
   
   /**
    * All possible states of the key
    */
-  type Event = 'keyDown' | 'keyUp'
+  type Event = 'keydown' | 'keyup'
   
   /**
    * Block of code taken from the Astreoid FRP example, simply observes what key is being pressed
@@ -124,6 +153,10 @@ function main() {
    * @returns A stream of keyboard inputs
    */
   const
+    // FPS setter
+    gameClock = interval(20).
+      pipe(map(elapsed => new Tick(elapsed))),
+
     keyPressed = <T>(e:Event, k:Key, result:()=>T)=>
     fromEvent<KeyboardEvent>(document,e)
       .pipe(
@@ -132,36 +165,48 @@ function main() {
         map(result)),
     
     //All possible movements
-    moveLeft = keyPressed('keyDown', 'ArrowLeft' || 'a', () => new Move(-Constants.playerMoveDis, 0)),
-    moveRight = keyPressed('keyDown', 'ArrowRight' || 'd', () => new Move(Constants.playerMoveDis, 0)),
-    moveUp = keyPressed('keyDown', 'ArrowUp' || 'w', () => new Move(0, Constants.playerMoveDis)),
-    moveDown = keyPressed('keyDown', 'ArrowDown' || 's', () => new Move(-Constants.playerMoveDis, 0)),
-    moveIdle = keyPressed('keyUp', 'ArrowLeft' && 'ArrowRight' && 'ArrowUp' && 'ArrowDown' && 'a' && 'd' && 'w' && 's', () => new Move(0,0))
-  
+    moveLeft = keyPressed('keydown', 'KeyA', () => new Move(-Constants.playerMoveDis, 0)),
+    moveRight = keyPressed('keydown', 'KeyD', () => new Move(Constants.playerMoveDis, 0)),
+    moveUp = keyPressed('keydown', 'KeyW', () => new Move(0, -Constants.playerMoveDis)),
+    moveDown = keyPressed('keydown', 'KeyS', () => new Move(0, Constants.playerMoveDis))
+
+  /**
+   * For moving any bodies that isn't controlled by the player, only works horizontally
+   * Code derived from the Astreoid FRP
+   * 
+   * @param curBody The current body being managed
+   * @returns 
+   */
+  const moveBody = (curBody: Body) => <Body>
+  {
+    ...curBody,
+    pos_x: curBody.pos_x + curBody.speed
+  }
+
   /**
    * Merges all game states into one stream
    * Derived from the Astreoid FRP
    */
   const mainGameStream = merge(
+    gameClock,
     moveLeft,
     moveRight,
     moveUp,
     moveDown,
-    moveIdle
   ).
     pipe(scan(currentState, initialState)).
-    subscribe(updateView) //continue later => make update view
-
+    subscribe(updateView);
+  
   /**
    * "Update is called once per frame"
    * Derived from the Astreoid FRP
    * @param theState The current state to update to the next frame
    */
-    function updateView(theState: State)
+  function updateView(theState: State)
   {
     const
       // Get the canvas to be able to refer to the elements
-      svg = document.getElementById("svgCanvas")!,
+      //svg = document.getElementById("svgCanvas")!,
 
       //Update all bodies on screen
       updateBodyView = (body: Body, canvas: HTMLElement) => 
@@ -179,6 +224,18 @@ function main() {
           canvas.appendChild(updateBody);
           return updateBody;
         }
+        
+        // const updateBody = (elem: Element, shape: String) =>
+        // {
+        //   shape == "ellipse" ?
+        //   updateBody
+        //   elem.setAttribute("cx", String(body.pos_x))
+        //   elem.setAttribute("cy", String(body.pos_y))
+        //   elem.setAttribute("rx", String(20))   //this is hardcoded... perish
+        //   elem.setAttribute("ry", String(20))   //this is hardcoded... perish
+        //   :
+        //   elem.
+        // }
         
         const updateBody = document.getElementById(body.ID) || createBodyView()
 
@@ -200,16 +257,22 @@ function main() {
 if (typeof window !== "undefined") {
   window.onload = () => {
     main();
-  };
+  }
 }
 
 /** Latest progress
- *  + Making 'updateview' (line 173)
+ *  + Turning the 'updateBodyView' to be more modular (Line 228)
+ *  + Might be able to be done by somply having two 'updateBodyView' one for the circle and the other for squares
  * 
  *  Trello but not really
- *  1. Make the whole fuckin engine i guess
- *  2. Player moves
- *  3. Enemy object moves
+ *  + Enemy object moves
+ *  + Clamp screen
+ *  + Make the enemy works like a train
+ *  + Give independent bodies the ability to hide and unhide on command
+ * 
+ *  Done:
+ *  + Implement engine
+ *  + Player Movement
  * 
  *  Note:
  *  1. Crocodile: can stand on its body, but not the head
@@ -217,4 +280,14 @@ if (typeof window !== "undefined") {
  *  3. Snake: should show up in later diffs, goes across the safe area
  *  4. There is actually no randomness! All rows has a set pattern
  *  5. Nothing moves in a grid, this even includes the frog
+ * 
+ *  Report:
+ *  In order to move a circle, you first make the universe. The first thing to make is 
+ *  the frame updating cycle, this is done between the 'mainGameStream' and 'updateView'. 'mainGameStream' 
+ *  merges all possible actions that can be done in the game into the 'currentState' to create the next state
+ *  ('initialState'), then passes it to the 'updateView' to render what that next state should look like. This
+ *  is done so that everything in the game can be updated only once per *frame
+ * 
+ *  *frame refers to the interaction between 'gameClock' and 'tick', where 'gameClock' makes a stream of 'tick'
+ *   with the given interval (which right now is set to 20ms to represent how long a frame shows up in ≈60 FPS)
  */
